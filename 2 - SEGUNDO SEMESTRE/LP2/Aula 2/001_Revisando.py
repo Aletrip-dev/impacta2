a75edb5f2585ae9d61f58ee2d3954817ebe7ebfc
Aletrip-dev <aletrip@msn.com>
2020-02-25 15:31:51 -0300

    Adicionado a frase "Teste GIT"

---
+++ 2 - SEGUNDO SEMESTRE/LP2/Aula 2/001_Revisando.py
@@ -0,0 +1,16 @@
+'''Valor = 10
+
+def Debito(Valor):
+    Valor = Valor * 2
+    return Valor
+
+ValorFinanceiro = 18
+print(Debito(ValorFinanceiro))
+
+'''
+
+num = int(input('Informe um número'))
+while num < 10:
+    print ("-"*100) 
+    num = int(input('Informe um número'))
+print ("Fim")
